07717106b7a157740ec7bd795141cd1a9dfe2f21
220235cs11 <113498966+220235cs11@users.noreply.github.com>
2022-09-15 16:09:01 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Lab1
+//
+//  Created by Abdulbosit Muxitdinov on 15/09/22.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    cout<<"Abdulbosit\n";
+    cout<<4*6<<endl;
+    return 0;
+}
